19bb5622507715f8e805626a75276870894a1efc
akbarkumari929 <akbarkumari929@gmail.com>
2025-03-17 18:54:53 +0000

    Add 13.js

---
+++ src/13.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  const data = {
+    message: 'Hello World'
+  };
+  res.json(data);
+});
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
