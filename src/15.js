5ec5d00c5f083fe14f0d7048de0bdcee02affed2
akbarkumari929 <akbarkumari929@gmail.com>
2025-03-19 06:38:05 +0000

    Add 15.js

---
+++ src/15.js
@@ -0,0 +1,10 @@
+const getRandomNodeJsCode = () => {
+  const code = `
+  // Get a random number between 1 and 10
+  const randomNumber = Math.floor(Math.random() * 10) + 1;
+  
+  // Print the result to the console
+  console.log(randomNumber);
+  `;
+  return code;
+}
